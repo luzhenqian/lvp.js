81b441da8b5f3ffd3f756ed69fe41dea61f6210e
luzhenqian <15753140326@163.com>
2020-03-10 13:32:59 +0800

    fix: test isFibonacci

--- test/lvp.test.js
+++ test/lvp.test.js
@@ -3,10 +3,12 @@ var myRules = [
   {
     name: "isFibonacci",
     logic: function(value) {
-      if (value == 0) {
-        return 0;
-      } else if (value == 1) {
-        return 1;
+      function fibonacci() {
+        if (value == 0) {
+          return 0;
+        } else if (value == 1) {
+          return 1;
+        }
       }
       return fibonacci(value - 1) + fibonacci(value - 2);
     }
